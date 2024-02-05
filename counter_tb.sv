986dd6410ca2936c4128a3d84b888142ec5fa75e
Prakhar Gupta <visitprakhar@gmail.com>
2024-02-04 18:51:57 -0600

    tasking in test-bench

--- counter_tb.sv
+++ counter_tb.sv
@@ -5,9 +5,39 @@ module counter_tb ();
     logic [3:0] wout;
     
     //tasks
-    
-    
-    
+    task init();
+        clk <=0;
+        rst <=1;
+        en <= 1;
+        dir <=1;
+        ld <=0;
+        $display("Initializing module");
+    endtask
+
+
+    task stim1();
+        $display("Beginning forward counting");
+        rst<=0;
+    endtask
+   
+    task stim2();
+        $display("Testing hold");
+        en<=0;
+    endtask
+
+    task stim3();
+        $display("Beginning reverse counting");
+        en<=1;
+        dir<=0;
+    endtask
+
+    task loadTest1();
+        $display("Checking load values");
+        ld<=1;
+        ld_val <=4'b0101;
+        #10 ld <=0;//turn off after half clock cycle
+    endtask
+
     //instantiating design under test
     counter COUNTER(
          .clk(clk), .rst(rst), .ld(ld), .ld_val(ld_val), .dir(dir), .en(en), .count(wout)
@@ -15,21 +45,20 @@ module counter_tb ();
 
 
     //setting clock
+    //
     always #10 clk = ~clk;
 
 
     initial begin
         //Dump waveforms
         $dumpfile("dump.vcd");
-        clk <=0;
-        rst <= 1;
-        en <= 1;
-        dir <=1;
-        ld  <=0;
-        
-        #10 
-        rst<=0;
-        
+        init();
+
+        #10 stim1();
+        #90 stim2();
+        #150 stim3();
+        #200 loadTest1();
+
                      
     end
 
